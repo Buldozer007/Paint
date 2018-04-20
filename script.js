7b9be9f256538cbea3687299cfed79f3188356b7
Your Name <you@example.com>
2018-04-20 19:31:46 +0400

    I did it

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-function setup(){   
-    createCanvas(1900,900);
-    background("#acacac");
+var socket = io.connect('http://localhost:3000');
+function setup() {
+    createCanvas(1900, 900);
+    background('#acacac');
 }
-function mouseDragged(){
-    noStroke();
-    fill(200,0,0);
-    ellipse(mouseX,mouseY,25,25);
+function mouseDragged() {
+    socket.emit('coords to server', [mouseX, mouseY]);
 }
-function main() {
-    var socket = io.connect('http://localhost:3000');
-    var canvas = document.getElementById('defaultCanvas0');
-    var input = document.getElementById('ellipse');
 
-    function handleClick(evt) {
-        var val = input.value;
-        if (val != "") {
-            socket.emit("add ellipse", val);
-        }
-    }
-   onclick = handleClick;
+socket.on("sending coords", drawEllipse);
+
+function drawEllipse(data) {
+
+    noStroke();
+    fill('blue');
+    ellipse(data[0], data[1], 25,25);
+
 }
-    
- 
+
--- server.js
+++ server.js
@@ -2,12 +2,22 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var ellipses = [];
+var ellipsesCoordinates = [];
 
 app.use(express.static("."));
 app.get('/', function (req, res) {
-   res.redirect('index.html');
+    res.redirect('index.html');
 });
 server.listen(3000);
- 
-//console.log(ellipses);
+
+io.on('connection', function (socket) {
+    for (var i in ellipsesCoordinates) {
+        io.sockets.emit("sending coords", ellipsesCoordinates[i]);
+    }
+    socket.on('coords to server', function (data) {
+        ellipsesCoordinates.push(data)
+        io.sockets.emit("sending coords", data);
+
+    }
+    );
+});
